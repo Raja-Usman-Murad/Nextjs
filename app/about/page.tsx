acdce4939486b577c54b9e83d3440f991217ae4d
rajausmanmurad <usmankiyani501@gmail.com>
2024-05-23 14:59:16 +0500

    set the page meta data by metadata and meta data function for dynamic

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,9 @@
 import React from "react";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Page",
+};
 
 const About = () => {
   return <div>About Page</div>;
--- app/lists/[listId]/page.tsx
+++ app/lists/[listId]/page.tsx
@@ -1,6 +1,20 @@
 import React from "react";
+import type { Metadata } from "next";
 
-const ListsDetails = ({ params }: { params: { listId: string } }) => {
+type Props = {
+  params: {
+    listId: string;
+  };
+};
+// we can do async calls as well in this function
+export const generateMetadata = ({ params }: Props): Metadata => {
+  const title = `list ${params.listId} ❤`;
+  return {
+    title: title,
+  };
+};
+
+const ListsDetails = ({ params }: Props) => {
   return <div>ListsDetails page {params.listId}</div>;
 };
 
