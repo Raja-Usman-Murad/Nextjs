aac462f41c2273902b173f7b47fb9bb3b5328204
rajausmanmurad <usmankiyani501@gmail.com>
2024-05-24 15:01:03 +0500

    parallel routes concept, slots

---
+++ app/complex-dashboard/@users/page.tsx
@@ -0,0 +1,8 @@
+import Card from "@/app/Components/UI/Card/Card";
+import React from "react";
+
+const UsersSlot: React.FC = () => {
+  return <Card>UsersSlot</Card>;
+};
+
+export default UsersSlot;
---
+++ app/complex-dashboard/layout.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const ComplexDashboardLayout = ({
+  children,
+  notifications,
+  revenue,
+  users,
+}: {
+  children: React.ReactNode;
+  notifications: React.ReactNode;
+  revenue: React.ReactNode;
+  users: React.ReactNode;
+}) => {
+  return (
+    <>
+      <div>{children}</div>
+      <div className="flex">
+        <div className="flex flex-col">
+          <div>{users}</div>
+          <div>{revenue}</div>
+        </div>
+        <div className="flex flex-1">{notifications}</div>
+      </div>
+    </>
+  );
+};
+
+export default ComplexDashboardLayout;
