d0eeaf283dadf5e174051776a87ddb10316615e5
rajausmanmurad <usmankiyani501@gmail.com>
2024-05-24 15:01:53 +0500

    handle the unmatched routes in parallel routes on reloading using default file

---
+++ app/complex-dashboard/@users/default.tsx
@@ -0,0 +1,8 @@
+import Card from "@/app/Components/UI/Card/Card";
+import React from "react";
+
+const DefaultUsersSlot: React.FC = () => {
+  return <Card>Default UsersSlot</Card>;
+};
+
+export default DefaultUsersSlot;
---
+++ app/complex-dashboard/default.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DefaultComplexDashboard: React.FC = () => {
+  return <div>Default Complex Dashboard</div>;
+};
+
+export default DefaultComplexDashboard;
