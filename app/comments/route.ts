6aa2f914f833cf3654b8597213fb8468e1436fe2
Raja Usman Murad <usmankiyani501@gmail.com>
2024-05-26 16:25:12 +0500

    query parameter in request

--- app/comments/route.ts
+++ app/comments/route.ts
@@ -1,7 +1,14 @@
+import { NextRequest } from "next/server";
 import { comments, commentsType } from "./commentsArray";
 
-export function GET() {
-  return Response.json(comments);
+export function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("query");
+
+  const filterComments = query
+    ? comments.filter((comment) => comment.text.includes(query))
+    : comments;
+  return Response.json(filterComments);
 }
 
 export async function POST(request: Request) {
