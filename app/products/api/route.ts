fbe8beb9a16972a9422893d3ec3fa816bca0ed6b
Raja Usman Murad <usmankiyani501@gmail.com>
2024-05-26 17:11:32 +0500

    headres in request

--- app/products/api/route.ts
+++ app/products/api/route.ts
@@ -1,3 +1,20 @@
-export function GET() {
-  return new Response("products from api");
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export function GET(request: NextRequest) {
+  //First Method using Web api
+  const requestHeader = new Headers(request.headers);
+  const authorization = requestHeader.get("Authorization");
+  console.log(authorization);
+
+  //Second Method using next/headers
+  const requestHeader2 = headers();
+  const authorization2 = requestHeader2.get("Authorization");
+  console.log(authorization2);
+
+  return new Response("<h1>products from api</h1>", {
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
 }
